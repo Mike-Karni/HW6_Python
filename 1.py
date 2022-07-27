3320c3f2fb08598cc5fed1ecb08a6d36eed82cb1
mike_karni <3923499@mail.ru>
2022-07-27 21:21:45 +0300

    SecondTask

---
+++ 1.py
@@ -0,0 +1,9 @@
+def same_by(characteristic, objects):
+  return len(set(map(characteristic, objects))) < 2
+values = [1,1,1,2]
+print(same_by(lambda x:x%2,values))
+values = [2,2,2,3]
+print(same_by(lambda x:x%2,values))
+
+
+
