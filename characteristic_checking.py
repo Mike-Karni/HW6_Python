d22bb4020716604adc73aef7859c2453383711a7
mike_karni <3923499@mail.ru>
2022-07-31 10:32:55 +0300

    HW DONE

--- characteristic_checking.py
+++ characteristic_checking.py
@@ -1,6 +1,6 @@
 def same_by(characteristic, objects):
   return len(set(map(characteristic, objects))) < 2
-values = [1,1,1,2]
+values = [1,1,1,1]
 print(same_by(lambda x:x%2,values))
 values = [2,2,2,3]
 print(same_by(lambda x:x%2,values))
